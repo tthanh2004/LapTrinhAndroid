52390550ff80cfc0dee8a1da524b211ec89ae41e
Thành Nguyễn <thanhtlu2k44@gmail.com>
2025-12-06 16:15:28 +0700

    Chuc nang cai dat chuyen di

--- vesiao_safetrek/lib/main.dart
+++ vesiao_safetrek/lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'common/constants.dart';
+import 'controllers/trip_controller.dart';
 import 'screens/mobile/mobile_screen.dart';
 
 void main() {
   runApp(
-    const SafeTrekApp(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TripController()),
+      ],
+      child: const SafeTrekApp(),
+    ),
   );
 }
 
 class SafeTrekApp extends StatelessWidget {
   const SafeTrekApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
