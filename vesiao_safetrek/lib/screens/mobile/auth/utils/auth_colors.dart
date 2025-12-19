966fdcea2580b2f4104374714c68cdac0874018a
AnhTuan250104 <nguyenanhtuan25012004@gmail.com>
2025-12-19 09:58:50 +0700

    update login, validate nhap

---
+++ vesiao_safetrek/lib/screens/mobile/auth/utils/auth_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthColors {
+  static const Color gradientStart = Color(0xFF3B82F6);
+  static const Color gradientEnd = Color(0xFF1D4ED8);
+  static const Color primaryLight = Color(0xFFEFF6FF); 
+  static const Color infoTextBlue = Color(0xFF1E40AF);
+  static const Color inputFill = Color(0xFFF9FAFB);
+  static const Color border = Color(0xFFE5E7EB);
+  static const Color buttonDisabled = Color(0xFFD1D5DB);
+  static const Color otpButtonBg = Color(0xFFFEF9C3);
+  static const Color otpButtonText = Color(0xFF854D0E);
+}
