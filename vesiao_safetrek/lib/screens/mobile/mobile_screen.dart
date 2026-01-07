a3a33968b792821bb52ade1b8671cebcdb8ab613
Thành Nguyễn <thanhtlu2k44@gmail.com>
2026-01-07 13:06:43 +0700

    them khan cap khi go vao man hinh

--- vesiao_safetrek/lib/screens/mobile/mobile_screen.dart
+++ vesiao_safetrek/lib/screens/mobile/mobile_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart'; // Thêm import provider
+import 'package:provider/provider.dart'; 
 import '../../common/constants.dart';
-import '../../controllers/trip_controller.dart'; // Thêm import controller
+import '../../controllers/trip_controller.dart'; 
 import 'home/home_tab.dart';
 import 'trip/trip_tab.dart';
 import 'contacts/contacts_tab.dart';
 import 'settings/settings_tab.dart';
+import '../../widgets/triple_tap_wrapper.dart';
 
 class MobileScreen extends StatefulWidget {
   final int userId;
@@ -27,6 +28,7 @@ class _MobileScreenState extends State<MobileScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Định nghĩa danh sách các Tab
     final List<Widget> tabs = [
       HomeTab(
         userId: widget.userId,
@@ -38,50 +40,53 @@ class _MobileScreenState extends State<MobileScreen> {
       SettingsTab(userId: widget.userId), 
     ];
 
-    // Dùng Consumer để lắng nghe trạng thái giám sát từ TripController
-    return Consumer<TripController>(
-      builder: (context, tripController, child) {
-        return Scaffold(
-          backgroundColor: Colors.white,
-          body: IndexedStack(
-            index: _currentIndex,
-            children: tabs,
-          ),
-          // NẾU ĐANG GIÁM SÁT THÌ GÁN NULL (ẨN THANH ĐIỀU HƯỚNG)
-          bottomNavigationBar: tripController.isMonitoring 
-            ? null 
-            : BottomNavigationBar(
-                currentIndex: _currentIndex,
-                onTap: _onTabTapped,
-                type: BottomNavigationBarType.fixed,
-                selectedItemColor: kPrimaryColor,
-                unselectedItemColor: Colors.grey,
-                showUnselectedLabels: true,
-                items: const [
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home_outlined),
-                    activeIcon: Icon(Icons.home),
-                    label: "Trang chủ",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.location_on_outlined),
-                    activeIcon: Icon(Icons.location_on),
-                    label: "Chuyến đi",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.people_outline),
-                    activeIcon: Icon(Icons.people),
-                    label: "Liên lạc",
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.settings_outlined),
-                    activeIcon: Icon(Icons.settings),
-                    label: "Cài đặt",
-                  ),
-                ],
-              ),
-        );
-      },
+    // 1. Bọc ngoài cùng bằng TripleTapWrapper để nhận diện gõ 3 lần toàn màn hình
+    return TripleTapWrapper(
+      userId: widget.userId,
+      child: Consumer<TripController>(
+        builder: (context, tripController, child) {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            body: IndexedStack(
+              index: _currentIndex,
+              children: tabs,
+            ),
+            // 2. NẾU ĐANG GIÁM SÁT THÌ ẨN THANH ĐIỀU HƯỚNG (Gán null)
+            bottomNavigationBar: tripController.isMonitoring 
+              ? null 
+              : BottomNavigationBar(
+                  currentIndex: _currentIndex,
+                  onTap: _onTabTapped,
+                  type: BottomNavigationBarType.fixed,
+                  selectedItemColor: kPrimaryColor,
+                  unselectedItemColor: Colors.grey,
+                  showUnselectedLabels: true,
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home_outlined),
+                      activeIcon: Icon(Icons.home),
+                      label: "Trang chủ",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.location_on_outlined),
+                      activeIcon: Icon(Icons.location_on),
+                      label: "Chuyến đi",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.people_outline),
+                      activeIcon: Icon(Icons.people),
+                      label: "Liên lạc",
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.settings_outlined),
+                      activeIcon: Icon(Icons.settings),
+                      label: "Cài đặt",
+                    ),
+                  ],
+                ),
+          );
+        },
+      ),
     );
   }
 }
